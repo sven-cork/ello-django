effc893ff8fbd5ef9359305b91cfed90eb427747
sven-cork <>
2023-05-20 17:10:11 +0000

    possible close to final commit

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if development: 
+if development:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 else:
