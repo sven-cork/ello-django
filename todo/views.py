7d15fc487026c2d85209bfa34b699f37c8118607
sven-cork <>
2023-05-20 13:19:48 +0000

    adjust formatting in views.py

--- todo/views.py
+++ todo/views.py
@@ -14,7 +14,7 @@ def get_todo_list(request):
 
 
 def add_item(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = ItemForm(request.POST)
         if form.is_valid():
             form.save()
@@ -28,7 +28,7 @@ def add_item(request):
 
 def edit_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
-    if request.method == "POST":
+    if request.method == 'POST':
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
